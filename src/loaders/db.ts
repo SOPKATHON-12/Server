f089dab8526c88b4e66b2903ac655f9779c113b5
hyejungg <kimhyejung12@naver.com>
2022-05-22 02:54:32 +0900

    [feat] Game Schema 생성

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose';
 import config from '../config';
+import Game from '../models/Game';
 
 const connectDB = async () => {
   try {
     await mongoose.connect(config.mongoURI);
 
     mongoose.set('autoCreate', true);
+
+    Game.createCollection().then(function (collection) {
+      console.log('Game Collection is created!');
+    });
 
     console.log('Mongoose Connected ...');
   } catch (err: any) {
