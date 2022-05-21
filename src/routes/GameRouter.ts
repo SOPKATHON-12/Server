720439a552e190e8158f1d756f15fcfeb952a78a
Yunjoo Son <thsduswn4141@naver.com>
2022-05-22 06:51:08 +0900

    [feat] API 작업 : 게임(데시벨, 탭) 랭킹 10개 조회

--- src/routes/GameRouter.ts
+++ src/routes/GameRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import GameController from '../controllers/GameController';
+import RankingController from '../controllers/RankingController';
 
 const router: Router = Router();
 router.post('/', GameController.createGame);
+router.get('/ranking', RankingController.getRanking);
 
 export default router;
